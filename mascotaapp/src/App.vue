57c709dfe27ef1637c231ab7279f46d190282b90
florenciasoria <fsoria57@gmail.com>
2021-10-24 16:23:56 -0300

    Agrego el boton solicitudes en App.vue

--- mascotaapp/src/App.vue
+++ mascotaapp/src/App.vue
@@ -5,7 +5,8 @@
 
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
-      <router-link to="/quieroAdoptar">Quiero</router-link>
+      <router-link to="/quieroAdoptar">Quiero</router-link> | 
+      <router-link to="/misSolicitudes">Solicitudes</router-link> | 
       
     </div>
     <router-view/>

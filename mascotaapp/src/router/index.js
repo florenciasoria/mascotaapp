e42f2d30b6dba3cc4350a253e2d137d5bc2382e6
florenciasoria <fsoria57@gmail.com>
2021-10-24 17:18:29 -0300

    Agrego la ruta de misPublicaciones en index.js

--- mascotaapp/src/router/index.js
+++ mascotaapp/src/router/index.js
@@ -35,6 +35,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/MisSolicitudes.vue')
   }
+  ,  
+  {
+    path: '/misPublicaciones',
+    name: 'Publicaciones',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/MisPublicaciones.vue')
+  }
 ]
 
 const router = new VueRouter({

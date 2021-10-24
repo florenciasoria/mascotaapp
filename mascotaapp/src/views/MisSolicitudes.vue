d1cb3595503664f771fcc4fc38067143ed40c8a0
florenciasoria <fsoria57@gmail.com>
2021-10-24 16:17:56 -0300

    Agrego el componente Solicitudes

---
+++ mascotaapp/src/views/MisSolicitudes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="solicitudes">
+    <Solicitudes msg="Mis Solicitudes"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Solicitudes from '@/components/Solicitudes.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Solicitudes
+  }
+}
+</script>

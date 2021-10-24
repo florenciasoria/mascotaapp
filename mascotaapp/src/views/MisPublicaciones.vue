e31851b00b9131de934527c51f8c138b4c36c795
florenciasoria <fsoria57@gmail.com>
2021-10-24 17:36:52 -0300

    Termino vista misPublicaciones

---
+++ mascotaapp/src/views/MisPublicaciones.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="publicaciones">
+    <Publicaciones msg="Mis Publicaciones"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Publicaciones from '@/components/Publicaciones.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Publicaciones
+  }
+}
+</script>

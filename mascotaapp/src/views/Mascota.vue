ed084adb7a72dbb78fd45a9643d4cccb3a219fee
agcaf96 <a.cafiero@outlook.com>
2021-10-29 20:43:55 -0300

    Acomodé dos nombres pavotes

--- mascotaapp/src/views/Mascota.vue
+++ mascotaapp/src/views/Mascota.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="mascota">
         <Mascota/>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-.home {
+.mascota {
     background-color: #e2aee4;
 }
 </style>

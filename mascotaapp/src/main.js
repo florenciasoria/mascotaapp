aff7742257f8bb6ddab2e1da4aecfd1705d392a5
agcaf96 <a.cafiero@outlook.com>
2021-10-30 17:41:29 -0300

    Metimos vueX

--- mascotaapp/src/main.js
+++ mascotaapp/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import router from './router'
-
+import router from './router';
+import store from './store'; 
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
---
+++ mascotaapp/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import mascotas from '../assets/js/mascotas.js'
+import usuarios from '../assets/js/users.js'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+        // esto le pone valor inicial a la variable mascotas (el array)
+        mascotas,
+        usuarios
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    },
+    getters:{
+
+    }
+
+}); 

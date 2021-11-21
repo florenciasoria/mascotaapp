423231ca717dd0677df80c0ea13fab0b061f3cc1
FedeMorgenstern <fede87morgan@gmail.com>
2021-11-21 19:33:00 -0300

    traer cosas de api una sola vez

--- mascotaapp/src/views/PanelAdministracion.vue
+++ mascotaapp/src/views/PanelAdministracion.vue
@@ -3,56 +3,69 @@
     <h3>Datos MascotaAPP</h3>
     <div class="container">
       <b-row>
-        <h4 class="mx-auto">Publicaciones</h4>
-      </b-row>
-      <b-row>
-        <b-col class="col-6">
-          <grafico-linea
-            v-if="datosCargados"
-            v-bind:datos="datosMascotaAlta"
-          ></grafico-linea>
-        </b-col>
-        <b-col class="col-6">
-          <grafico-torta
-            v-if="datosCargados3"
-            v-bind:datos="datosMascotaEspecie"
-          ></grafico-torta>
+        <b-col class="mx-auto">
+          <button @click="cargarGraficosPublicaciones">Publicaciones</button>
+          <button @click="cargarGraficosSolicitudes">Solicitudes</button>
+          <button @click="cargarGraficosAdopciones">Adopciones</button>
         </b-col>
       </b-row>
-      <b-row>
-        <h4 class="mx-auto">Solicitudes</h4>
-      </b-row>
-      <b-row>
-        <b-col class="col-6">
-          <grafico-linea
-            v-if="datosCargadosLineaSolicitudes"
-            v-bind:datos="datosMascotaSolicitudes"
-          ></grafico-linea>
-        </b-col>
-        <b-col class="col-6">
-          <grafico-torta
-            v-if="datosCargadosTortaSolicitudes"
-            v-bind:datos="datosMascotaEspecieSolicitudes"
-          ></grafico-torta>
-        </b-col>
-      </b-row>
-      <b-row>
-        <h4 class="mx-auto">Adopciones</h4>
-      </b-row>
-      <b-row>
-        <b-col class="col-6">
-          <grafico-linea
-            v-if="datosCargadosLineaAdopcion"
-            v-bind:datos="datosMascotaAdopcion"
-          ></grafico-linea>
-        </b-col>
-        <b-col class="col-6">
-          <grafico-torta
-            v-if="datosCargadosTortaAdopciones"
-            v-bind:datos="datosMascotaEspecieAdopciones"
-          ></grafico-torta>
-        </b-col>
-      </b-row>
+      <div v-if="vistaPublicaciones">
+        <b-row>
+          <h4 class="mx-auto">Publicaciones</h4>
+        </b-row>
+        <b-row>
+          <b-col class="col-6">
+            <grafico-linea
+              v-if="datosCargados"
+              v-bind:datos="datosMascotaAlta"
+            ></grafico-linea>
+          </b-col>
+          <b-col class="col-6">
+            <grafico-torta
+              v-if="datosCargados3"
+              v-bind:datos="datosMascotaEspecie"
+            ></grafico-torta>
+          </b-col>
+        </b-row>
+      </div>
+      <div v-if="vistaSolicitudes">
+        <b-row>
+          <h4 class="mx-auto">Solicitudes</h4>
+        </b-row>
+        <b-row>
+          <b-col class="col-6">
+            <grafico-linea
+              v-if="datosCargadosLineaSolicitudes"
+              v-bind:datos="datosMascotaSolicitudes"
+            ></grafico-linea>
+          </b-col>
+          <b-col class="col-6">
+            <grafico-torta
+              v-if="datosCargadosTortaSolicitudes"
+              v-bind:datos="datosMascotaEspecieSolicitudes"
+            ></grafico-torta>
+          </b-col>
+        </b-row>
+      </div>
+      <div v-if="vistaAdopciones">
+        <b-row>
+          <h4 class="mx-auto">Adopciones</h4>
+        </b-row>
+        <b-row>
+          <b-col class="col-6">
+            <grafico-linea
+              v-if="datosCargadosLineaAdopcion"
+              v-bind:datos="datosMascotaAdopcion"
+            ></grafico-linea>
+          </b-col>
+          <b-col class="col-6">
+            <grafico-torta
+              v-if="datosCargadosTortaAdopciones"
+              v-bind:datos="datosMascotaEspecieAdopciones"
+            ></grafico-torta>
+          </b-col>
+        </b-row>
+      </div>
     </div>
   </div>
 </template>
@@ -145,6 +158,10 @@ export default {
       datosCargadosTortaAdopciones: false,
       datosCargadosTortaSolicitudes: false,
       datosCargadosLineaSolicitudes: false,
+
+      vistaPublicaciones: false,
+      vistaSolicitudes: false,
+      vistaAdopciones: false,
       //mascotasInicial: [],
       mascotasAdoptadas: [],
       mascotasSolicitadas: [],
@@ -161,75 +178,14 @@ export default {
     // }
     //CARGA DE DATOS
     // obtengo mi array de fechas totales
+
     await this.obtenerArrayFechasCompleto();
-
-    // obtengo mascotas totales
-    this.mascotasInicial = await this.traerMascotasDeApi();
-
-    //obtengo mascotas aceptadas
-    this.solicitudesAceptadas = await this.traerSolicitudesByEstado(
-      valoresData.estadoSolicitud.aceptada
-    );
-
     // obtengo todas las solicitudes
     this.solicitudesTotales = await this.traerSolicitudes();
-
-    // busca mascotas aceptadas de la solicitud
-    await this.buscarMascotasDeSolicitud(
-      this.solicitudesAceptadas,
-      this.mascotasAdoptadas
-    );
-    //busco mascotas de la solicitud
-    await this.buscarMascotasDeSolicitud(
-      this.solicitudesTotales,
-      this.mascotasSolicitadas
-    );
-
-    //PUBLICACIONES
-    this.datosMascotaEspecie.arrayDatos = this.cargarGraficoEspecieTorta(
-      this.mascotasInicial
-    );
-    this.datosMascotaAlta.mascotasDatos.array = this.cargarGraficoLineaTotal(
-      this.mascotasInicial
-    );
-
-    this.datosMascotaAlta.perrosDatos.array =
-      this.cargarGraficoLineaPorEspecie("perro",this.mascotasInicial);
-
-    this.datosMascotaAlta.gatosDatos.array =
-      this.cargarGraficoLineaPorEspecie("gato",this.mascotasInicial);
-
-    //solicitudes
-    this.datosMascotaEspecieAdopciones.arrayDatos =
-      this.cargarGraficoEspecieTorta(this.mascotasSolicitadas);
-
-    this.datosMascotaSolicitudes.mascotasDatos.array =
-      this.cargarGraficoLineaTotal(this.mascotasSolicitadas);
-
-    this.datosMascotaSolicitudes.perrosDatos.array =
-      this.cargarGraficoLineaPorEspecie("perro",this.mascotasSolicitadas);
-    this.datosMascotaSolicitudes.gatosDatos.array =
-      this.cargarGraficoLineaPorEspecie("gato",this.mascotasSolicitadas);
-
-    //ADOPCIONES
-    this.datosMascotaAdopcion.mascotasDatos.array =
-      this.cargarGraficoLineaTotal(this.mascotasAdoptadas);
-
-    this.datosMascotaEspecieSolicitudes.arrayDatos =
-      this.cargarGraficoEspecieTorta(this.mascotasAdoptadas);
-
-    this.datosMascotaAdopcion.perrosDatos.array =
-      this.cargarGraficoLineaPorEspecie("perro",this.mascotasAdoptadas);
-
-    this.datosMascotaAdopcion.gatosDatos.array =
-      this.cargarGraficoLineaPorEspecie("gato",this.mascotasAdoptadas);
-
-    this.datosCargados = true;
-    this.datosCargadosLineaAdopcion = true;
-    this.datosCargadosLineaSolicitudes = true;
-    this.datosCargadosTortaAdopciones = true;
-    this.datosCargados3 = true;
-    this.datosCargadosTortaSolicitudes = true;
+    console.log("solicitudesTotales", this.solicitudesTotales);
+    // obtengo mascotas totales
+    this.mascotasInicial = await this.traerMascotasDeApi();
+    console.log("mascotas", this.mascotasInicial);
   },
 
   methods: {
@@ -237,6 +193,77 @@ export default {
     // validarRolDeUsuario() {
     //   return this.getusuariosLog().rol === 'z'
     // },
+
+    async cargarGraficosPublicaciones() {
+      //datos
+
+      //PUBLICACIONES
+      this.datosMascotaEspecie.arrayDatos = this.cargarGraficoEspecieTorta(
+        this.mascotasInicial
+      );
+      this.datosMascotaAlta.mascotasDatos.array = this.cargarGraficoLineaTotal(
+        this.mascotasInicial
+      );
+
+      this.datosMascotaAlta.perrosDatos.array =
+        this.cargarGraficoLineaPorEspecie("perro", this.mascotasInicial);
+
+      this.datosMascotaAlta.gatosDatos.array =
+        this.cargarGraficoLineaPorEspecie("gato", this.mascotasInicial);
+      this.vistaPublicaciones = true;
+      this.datosCargados = true;
+      this.datosCargados3 = true;
+    },
+    async cargarGraficosSolicitudes() {
+      //busco mascotas de la solicitud
+      await this.buscarMascotasDeSolicitud(
+        this.solicitudesTotales,
+        this.mascotasSolicitadas
+      );
+
+      //solicitudes
+      this.datosMascotaEspecieAdopciones.arrayDatos =
+        this.cargarGraficoEspecieTorta(this.mascotasSolicitadas);
+
+      this.datosMascotaSolicitudes.mascotasDatos.array =
+        this.cargarGraficoLineaTotal(this.mascotasSolicitadas);
+
+      this.datosMascotaSolicitudes.perrosDatos.array =
+        this.cargarGraficoLineaPorEspecie("perro", this.mascotasSolicitadas);
+      this.datosMascotaSolicitudes.gatosDatos.array =
+        this.cargarGraficoLineaPorEspecie("gato", this.mascotasSolicitadas);
+      this.vistaSolicitudes = true;
+      this.datosCargadosLineaSolicitudes = true;
+      this.datosCargadosTortaSolicitudes = true;
+    },
+
+    async cargarGraficosAdopciones() {
+      //obtengo mascotas aceptadas
+      this.solicitudesAceptadas = await this.traerSolicitudesByEstado(
+        valoresData.estadoSolicitud.aceptada
+      );
+
+      // busca mascotas aceptadas de la solicitud
+      await this.buscarMascotasDeSolicitud(
+        this.solicitudesAceptadas,
+        this.mascotasAdoptadas
+      );
+      //ADOPCIONES
+      this.datosMascotaAdopcion.mascotasDatos.array =
+        this.cargarGraficoLineaTotal(this.mascotasAdoptadas);
+
+      this.datosMascotaEspecieSolicitudes.arrayDatos =
+        this.cargarGraficoEspecieTorta(this.mascotasAdoptadas);
+
+      this.datosMascotaAdopcion.perrosDatos.array =
+        this.cargarGraficoLineaPorEspecie("perro", this.mascotasAdoptadas);
+
+      this.datosMascotaAdopcion.gatosDatos.array =
+        this.cargarGraficoLineaPorEspecie("gato", this.mascotasAdoptadas);
+      this.vistaAdopciones = true;
+      this.datosCargadosLineaAdopcion = true;
+      this.datosCargadosTortaAdopciones = true;
+    },
 
     async obtenerArrayFechasCompleto() {
       const primerFecha = await apiMascotas.getPrimerFecha();
@@ -255,7 +282,6 @@ export default {
       console.log("FECHAS", this.arrayFechas, "ACA!!!");
     },
 
- 
     cargarGraficoEspecieTorta(arrayMascotas) {
       const arrayGrafico = [];
       for (const mascota of arrayMascotas) {
@@ -352,41 +378,31 @@ export default {
         console.log(error.message);
       }
     },
-
-    async traerSolicitudesByEstado(estado) {
-      try {
-        return await (
-          await apiSolictudes.getByEstado(estado)
-        ).data;
-      } catch (error) {
-        console.log(error.message);
-      }
-    },
-
-    async buscarMascotasDeSolicitud(arraySolicitudes, arrayMascotas) {
-      try {
-        for (const s of arraySolicitudes) {
-          //esto nos trae todas las solicitudes de una mascota
-          const mascota = await (await apiMascotas.getById(s.idMascota)).data;
-          arrayMascotas.push({
-            // ... carga todos los datos de la mascota
-            ...s,
-            ...mascota,
-          });
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    },
-
     async traerMascotasDeApi() {
       try {
         const resuGet = await apiMascotas.get();
         const arrayMascotas = resuGet.data;
-        console.log("arrayMascotas", arrayMascotas);
+        //console.log("arrayMascotas", arrayMascotas);
         return arrayMascotas;
       } catch (error) {
         console.log(error.message);
+      }
+    },
+
+    traerSolicitudesByEstado(estado) {
+      return this.solicitudesTotales.filter((s) => s.estado == estado);
+    },
+
+    buscarMascotasDeSolicitud(arraySolicitudes, arrayMascotas) {
+      for (const s of arraySolicitudes) {
+        //esto nos trae todas las solicitudes de una mascota
+        //const mascota = await (await apiMascotas.getById(s.idMascota)).data;
+        const mascota = this.mascotasInicial.find((m) => m.id == s.idMascota);
+        arrayMascotas.push({
+          // ... carga todos los datos de la mascota
+          ...s,
+          ...mascota,
+        });
       }
     },
   },

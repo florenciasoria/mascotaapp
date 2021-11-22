da782613dd704f1b42eb74dfdd8074298f86a16f
FedeMorgenstern <fede87morgan@gmail.com>
2021-11-22 14:40:05 -0300

    elimino mascotas del store

--- mascotaapp/src/store/index.js
+++ mascotaapp/src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import mascotas from '../assets/js/mascotas.js'
 import usuariosLog from '../assets/js/usuariosLog.js'
 
 
@@ -8,22 +7,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        // esto le pone valor inicial a la variable mascotas (el array)
-        mascotas,
         usuariosLog
     },
     mutations: {
-
-        agregarmascota(state, mascota) {
-            state.mascotas.push(mascota);
-        },
-        eliminarmascota(state, id) {
-            state.mascotas = state.mascotas.filter((mascota) => mascota.id != id)
-        },
-        editarmascota(state, nuevoArrayDeMascotas) {
-            state.mascotas = nuevoArrayDeMascotas;
-        },
-
         agregarusuarioLog(state, usuario) {
             state.usuariosLog = usuario;
         },
@@ -44,9 +30,6 @@ export default new Vuex.Store({
 
     },
     getters: {
-        getmascotas: (state) => {
-            return state.mascotas
-        },
         getusuariosLog: (state) => {
             return state.usuariosLog
         }
